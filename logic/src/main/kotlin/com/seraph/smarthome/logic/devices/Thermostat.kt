86925518eda458933fec1ac3b4056927ab589812
aleksandr.naumov <fireserafim@gmail.com>
2018-12-03 16:07:38 +0300

    #12 Thermostat device added

---
+++ logic/src/main/kotlin/com/seraph/smarthome/logic/devices/Thermostat.kt
@@ -0,0 +1,23 @@
+package com.seraph.smarthome.logic.devices
+
+import com.seraph.smarthome.device.DeviceDriver
+import com.seraph.smarthome.domain.Endpoint
+import com.seraph.smarthome.domain.Types
+
+class Thermostat : DeviceDriver {
+
+    private val targetTemp = 30f
+    private val allowedDelta = 0.3f
+
+    override fun configure(visitor: DeviceDriver.Visitor) {
+        val sensorInput = visitor.declareInput("from_sensor", Types.FLOAT, Endpoint.Retention.NOT_RETAINED)
+        val switchOutput = visitor.declareOutput("to_switch", Types.BOOLEAN, Endpoint.Retention.NOT_RETAINED)
+
+        sensorInput.observe { value ->
+            when {
+                value < targetTemp - allowedDelta -> switchOutput.set(true)
+                value > targetTemp -> switchOutput.set(false)
+            }
+        }
+    }
+}
