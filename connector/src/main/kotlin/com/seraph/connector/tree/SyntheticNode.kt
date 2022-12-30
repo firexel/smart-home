d0b6e3f03b469b258ec8a7dcdd0394eaf21aa110
Alexandr Naumov <fireserafim@gmail.com>
2022-12-30 20:57:52 +0300

    Synthetic node fixed so every stream input changes every output

--- connector/src/main/kotlin/com/seraph/connector/tree/SyntheticNode.kt
+++ connector/src/main/kotlin/com/seraph/connector/tree/SyntheticNode.kt
@@ -7,12 +7,9 @@ import com.seraph.smarthome.domain.Endpoint
 import com.seraph.smarthome.domain.Serializer
 import com.seraph.smarthome.domain.Types
 import com.seraph.smarthome.util.Log
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import script.definition.Synthetic
 import script.definition.Units
 import java.util.concurrent.Executor
@@ -22,84 +19,89 @@ import kotlin.reflect.KClass
 class SyntheticNode<T : Any>(
     private val id: String,
     type: KClass<T>,
-    private val persistence: Synthetic.Persistence<T>,
+    persistence: Synthetic.Persistence<T>,
     private val access: Synthetic.ExternalAccess,
     private val units: Units,
-    private val storage: Storage,
+    storage: Storage,
     private val driverManager: DriversManager,
     private val log: Log
 ) : Synthetic<T>, Node {
 
-    val type: Endpoint.Type<T> =
-        SyntheticDeviceDriver.inferType(type)
-            ?: throw Node.NodeInitException("Unsupported type $type")
+    private val type: Endpoint.Type<T> = inferType(type)
+        ?: throw Node.NodeInitException("Unsupported type $type")
+
+    private val store: Store<T> = when (persistence) {
+        is Synthetic.Persistence.None -> NoneStore()
+        is Synthetic.Persistence.Runtime -> RuntimeStore(persistence.default)
+        is Synthetic.Persistence.Stored -> PersistedStore(persistence.default, this.type, storage)
+    }
 
     override val output = StateFlowProducerNode<T>(this, null)
     override val input = StateFlowConsumerNode<T>(this, null)
 
     override suspend fun run(scope: CoroutineScope) {
         scope.launch {
-            persistence.load()?.also { input.stateFlow.value = it }
+            store.load()?.also { input.stateFlow.value = it }
             val driver = registerDevice()
-            try {
-                input.stateFlow.filterNotNull().collect {
-                    withContext(NonCancellable) {
-                        driver.transmit(it)
-                        if (persistence !is Synthetic.Persistence.None) {
-                            storage.put("value", type.serializer.toBytes(it))
-                        }
-                    }
+            val jobs = listOf(
+                scope.launch {
+                    startReceivingDataFromTheGraph(driver)
+                },
+                scope.launch {
+                    startReceivingDataFromTheNetwork(driver)
                 }
+            )
+            try {
+                jobs.forEach { it.join() }
             } finally {
                 withContext(NonCancellable) { unregisterDevice() }
             }
         }
     }
 
-    private suspend fun Synthetic.Persistence<T>.load(): T? {
-        return when (this) {
-            is Synthetic.Persistence.Stored<T> -> {
-                try {
-                    storage.get("value")?.let { type.serializer.fromBytes(it) }
-                } catch (ex: Serializer.TypeMismatchException) {
-                    log.w("Value stored in storage cannot be cast to a $type")
-                    storage.put("value", default?.let { type.serializer.toBytes(it) })
-                    default
-                }
+    private suspend fun startReceivingDataFromTheNetwork(driver: SyntheticDeviceDriver<T>) {
+        driver.feedback.filterNotNull().collect {
+            withContext(NonCancellable) {
+                log.v("Network --[$it]-> Graph")
+                putReceivedValue(it, driver)
             }
-            is Synthetic.Persistence.Runtime<T> -> default
-            else -> null
         }
     }
 
-    private suspend fun registerDevice(): SyntheticDeviceDriver<T> {
-        val driver = SyntheticDeviceDriver(
-            access,
-            output.stateFlow,
-            type,
-            log,
-            units
-        )
-        driverManager.addDriver(Device.Id(id), driver, SyntheticDeviceDriver.executor)
+    private suspend fun startReceivingDataFromTheGraph(driver: SyntheticDeviceDriver<T>) {
+        input.stateFlow.filterNotNull().collect {
+            withContext(NonCancellable) {
+                log.v("Graph --[$it]-> Network")
+                putReceivedValue(it, driver)
+            }
+        }
+    }
+
+    private suspend fun putReceivedValue(data: T, driver: SyntheticDeviceDriver<T>) {
+        store.put(data)
+        output.value = data
+        driver.transmit(data)
+    }
+
+    private fun registerDevice(): SyntheticDeviceDriver<T> {
+        val driver = SyntheticDeviceDriver(MutableStateFlow(output.stateFlow.value), type)
+        driverManager.addDriver(Device.Id(id), driver, executor)
         return driver
     }
 
-    private suspend fun unregisterDevice() {
+    private fun unregisterDevice() {
         // unsupported
     }
 
-    private class SyntheticDeviceDriver<T : Any>(
-        private val access: Synthetic.ExternalAccess,
-        private val feedback: MutableStateFlow<T?>,
-        private val type: Endpoint.Type<T>,
-        private val log: Log,
-        units: Units
+    private inner class SyntheticDeviceDriver<T : Any>(
+        val feedback: MutableStateFlow<T?>,
+        private val type: Endpoint.Type<T>
     ) : DeviceDriver {
 
         val units = try {
-            com.seraph.smarthome.domain.Units.valueOf(units.name)
+            com.seraph.smarthome.domain.Units.valueOf(this@SyntheticNode.units.name)
         } catch (ex: IllegalArgumentException) {
-            log.w("Unknown units $units")
+            log.w("Unknown units ${this@SyntheticNode.units}")
             com.seraph.smarthome.domain.Units.NO
         }
 
@@ -109,33 +111,91 @@ class SyntheticNode<T : Any>(
             output?.set(data)
         }
 
-        override fun bind(visitor: DeviceDriver.Visitor) {
-            if (access == Synthetic.ExternalAccess.READ || access == Synthetic.ExternalAccess.READ_WRITE) {
-                output = visitor.declareOutput("out", type)
-                    .setUnits(units)
-            }
-            if (access == Synthetic.ExternalAccess.WRITE || access == Synthetic.ExternalAccess.READ_WRITE) {
-                visitor.declareInput("in", type)
-                    .setUnits(units)
-                    .observe { feedback.value = it }
+        override fun bind(visitor: DeviceDriver.Visitor) = when (access) {
+            Synthetic.ExternalAccess.READ ->
+                registerOutput(visitor)
+            Synthetic.ExternalAccess.WRITE ->
+                registerInput(visitor)
+            Synthetic.ExternalAccess.READ_WRITE -> {
+                registerOutput(visitor)
+                registerInput(visitor)
             }
         }
 
-        @Suppress("UNCHECKED_CAST")
-        companion object {
-
-            val executor: Executor = Executors.newFixedThreadPool(1)
-
-            fun <T : Any> inferType(klass: KClass<T>): Endpoint.Type<T>? {
-                return when (klass) {
-                    Float::class -> Types.FLOAT as Endpoint.Type<T>?
-                    Int::class -> Types.INTEGER as Endpoint.Type<T>?
-                    Boolean::class -> Types.BOOLEAN as Endpoint.Type<T>?
-                    String::class -> Types.STRING as Endpoint.Type<T>?
-                    Unit::class -> Types.ACTION as Endpoint.Type<T>?
-                    else -> null
+        private fun registerInput(visitor: DeviceDriver.Visitor) {
+            visitor.declareInput("in", type)
+                .setUnits(units)
+                .observe { data ->
+                    feedback.value = data
                 }
+        }
+
+        private fun registerOutput(visitor: DeviceDriver.Visitor) {
+            output = visitor.declareOutput("out", type)
+                .setUnits(units)
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    companion object {
+
+        val executor: Executor = Executors.newFixedThreadPool(1)
+
+        fun <T : Any> inferType(klass: KClass<T>): Endpoint.Type<T>? = when (klass) {
+            Float::class -> Types.FLOAT as Endpoint.Type<T>?
+            Int::class -> Types.INTEGER as Endpoint.Type<T>?
+            Boolean::class -> Types.BOOLEAN as Endpoint.Type<T>?
+            String::class -> Types.STRING as Endpoint.Type<T>?
+            Unit::class -> Types.ACTION as Endpoint.Type<T>?
+            else -> null
+        }
+    }
+
+    interface Store<T> {
+        suspend fun put(value: T?)
+        suspend fun load(): T?
+    }
+
+    class NoneStore<T> : Store<T> {
+        override suspend fun put(value: T?) {
+            // do nothing
+        }
+
+        override suspend fun load(): T? {
+            return null
+        }
+    }
+
+    class RuntimeStore<T>(private val defaultVal: T?) : Store<T> {
+        override suspend fun put(value: T?) {
+            // do nothing
+        }
+
+        override suspend fun load(): T? {
+            return defaultVal
+        }
+    }
+
+    inner class PersistedStore<T>(
+        private val defaultVal: T?,
+        private val type: Endpoint.Type<T>,
+        private val storage: Storage
+    ) : Store<T> {
+
+        override suspend fun put(value: T?) {
+            storage.put(STORAGE_KEY, value?.let(type.serializer::toBytes))
+        }
+
+        override suspend fun load(): T? {
+            return try {
+                storage.get(STORAGE_KEY)?.let { type.serializer.fromBytes(it) }
+            } catch (ex: Serializer.TypeMismatchException) {
+                log.w("Value stored in storage cannot be cast to a $type")
+                storage.put(STORAGE_KEY, defaultVal?.let(type.serializer::toBytes))
+                defaultVal
             }
         }
     }
 }
+
+private const val STORAGE_KEY = "value"
